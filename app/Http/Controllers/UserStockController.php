2adbda1eb6ba371bc30821650e1671e6cc771fbe
Muhammad Safei Yahya <msafeiyahya96@gmail.com>
2026-02-27 16:18:47 +0700

    [fix] hide inactive items in the item stock page.

--- app/Http/Controllers/UserStockController.php
+++ app/Http/Controllers/UserStockController.php
@@ -7,13 +7,30 @@ use Illuminate\Http\Request;
 
 class UserStockController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $entriesPage = $request->entries ?? 10;
+        $search = $request->search ?? '';
+
         // $transaction = Transaction::where('pic_approval', 'Pending')->get();
-        $items = Items::all();
+        $baseQueryItems = Items::where('active', '!=', 'N')
+            ->orWhereNull('active');
+
+        if ($search)
+            $baseQueryItems->where(function ($query) use ($search) {
+                $query->where('item_code', 'LIKE', '%' . $search . '%')
+                    ->orWhere('item_name', 'LIKE', '%' . $search . '%')
+                    ->orWhere('item_unit', 'LIKE', '%' . $search . '%')
+                    ->orWhere('item_stock', 'LIKE', '%' . $search . '%');
+            });
+
+        $items = $baseQueryItems->paginate($entriesPage);
+
         return view('main.stock.index', [
-            "title" =>  "Item Stock",
-            "items" => $items
+            "title" => "Item Stock",
+            "items" => $items,
+            "entriesPage" => $entriesPage,
+            "search" => $search,
         ]);
     }
 }
